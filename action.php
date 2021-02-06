75dfab4294e33246c816a54476dbd3dfb9948aa1
nurulhidayahbs1 <78643073+nurulhidayahbs1@users.noreply.github.com>
2021-02-06 18:58:32 +0800

    Add files via upload

---
+++ action.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Pencegahan</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+       <link rel="stylesheet" href="css/owl.carousel.min.css"> 
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <link rel="stylesheet" href="https://rawgit.com/LeshikJanz/libraries/master/Bootstrap/baguetteBox.min.css">
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   </head>
+   <!-- body -->
+   <body class="main-layout inner_page">
+      <!-- loader  -->
+      <div class="loader_bg">
+         <div class="loader"><img src="images/loading.gif" alt="#"/></div>
+      </div>
+      <!-- end loader -->
+      <!-- top -->
+                    <!-- header -->
+         <header class="header-area">
+            <div class="left">
+               <a href="Javascript:void(0)"><i class="fa fa-search" aria-hidden="true"></i></a>
+            </div>
+            <div class="right">
+               <a href="Javascript:void(0)"><i class="fa fa-user" aria-hidden="true"></i></a>
+            </div>
+            <div class="container">
+               <div class="row d_flex">
+                  <div class="col-sm-3 logo_sm">
+                     <div class="logo">
+                        <a href="index.php"></a>
+                     </div>
+                  </div>
+                  <div class="col-lg-10 offset-lg-1 col-md-12 col-sm-9">
+                     <div class="navbar-area">
+                        <nav class="site-navbar">
+                           <ul>
+                              <li><a href="index.php">Home</a></li>
+                              <li><a class="active" href="action.php">Pencegahan</a></li>
+                              <li><a href="news.php">Tentang</a></li>
+                           </ul>
+                           <button class="nav-toggler">
+                           <span></span>
+                           </button>
+                        </nav>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </header>
+      <!-- end header -->
+    
+    <!-- cases -->
+      <div class="cases">
+         <div class="container-fluid">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage text_align_center ">
+                     <h2>CARA PENCEGAHAN</h2>
+                     <p>Untuk mencegah pencegahan Virus Corona tetap untuk mematuhi protokol kesehatan</p>
+                  </div>
+               </div>
+            </div>
+
+            <div class="protect">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-md-12">
+                        <div class="titlepage text_align_center">
+                        </div>
+                     </div>
+                  </div>
+               </div>
+                 <div class="protect_bg">
+               <div class="container">
+                   <div class="row">
+                     <div class="col-md-12">
+                        <!--  Demos -->
+                        <div class="owl-carousel owl-theme">
+                           <div class="item">
+                              <div class="protect_box text_align_center">
+                                <div class="desktop">
+                                   <i><img src="images/pro1.png" alt="#"/></i>
+                                 <h3> Memakai Masker</h3>
+                                 <span> Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for</span>
+                                </div>
+                              </div>
+                           </div>
+                           <div class="item">
+                                <div class="protect_box text_align_center">
+                                <div class="desktop">
+                                   <i><img src="images/pro2.png" alt="#"/></i>
+                                 <h3> Mencuci Tangan</h3>
+                                 <span> Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for</span>
+                                </div>
+                              </div>
+                           </div>
+                           <div class="item">
+                               <div class="protect_box text_align_center">
+                                <div class="desktop">
+                                   <i><img src="images/pro3.png" alt="#"/></i>
+                                 <h3> Dirumah Saja</h3>
+                                 <span> Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for</span>
+                                </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                </div>
+            </div>
+
+            <div class="row d_flex">
+               <div class=" col-md-4">
+         
+               </div>
+               <div class=" col-md-4">
+                 
+               </div>
+               <div class=" col-md-4">
+                 
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- end cases -->
+      <!--  footer -->
+      <footer>
+         <div class="footer">
+            <div class="container">
+               
+            </div>
+         </div>
+      </footer>
+      <!-- end footer -->
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
+      <script src="js/owl.carousel.min.js"></script>
+      <script src="js/custom.js"></script>
+   </body>
+</html>
---
+++ index.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Beranda</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+       <link rel="stylesheet" href="css/owl.carousel.min.css"> 
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <link rel="stylesheet" href="https://rawgit.com/LeshikJanz/libraries/master/Bootstrap/baguetteBox.min.css">
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   </head>
+   <!-- body -->
+   <body class="main-layout">
+      <!-- loader  -->
+      <div class="loader_bg">
+         <div class="loader"><img src="images/loading.gif" alt="#"/></div>
+      </div>
+      <!-- end loader -->
+      <!-- top -->
+                    <!-- header -->
+         <header class="header-area">
+            <div class="left">
+               <a href="Javascript:void(0)"><i class="fa fa-search" aria-hidden="true"></i></a>
+            </div>
+            <div class="right">
+               <a href="Javascript:void(0)"><i class="fa fa-user" aria-hidden="true"></i></a>
+            </div>
+            <div class="container">
+               <div class="row d_flex">
+                  <div class="col-sm-3 logo_sm">
+                     <div class="logo">
+                        <a href="index.php"></a>
+                     </div>
+                  </div>
+                  <div class="col-lg-10 offset-lg-1 col-md-12 col-sm-9">
+                     <div class="navbar-area">
+                        <nav class="site-navbar">
+                           <ul>
+                              <li><a class="active" href="index.php">Home</a></li>
+                              <li><a href="action.php">Pencegahan</a></li>
+                              <li><a href="news.php">Tentang</a></li>
+                           </ul>
+                           <button class="nav-toggler">
+                           <span></span>
+                           </button>
+                        </nav>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </header>
+      <!-- end header -->
+            <div class="full_bg">
+      <!-- header inner -->
+         <div class="section">
+            <!-- carousel code -->
+            <div id="banner1" class="carousel slide slider_main">
+               <ol class="carousel-indicators ">
+                  <li data-target="#banner1" data-slide-to="0" class="indicator-li-1">01</li>
+                  <li data-target="#banner1" data-slide-to="1" class="">02</li>
+                  <li data-target="#banner1" data-slide-to="2" class="active">03</li>
+               </ol>
+               <div class="carousel-inner">
+                  <!-- first slide -->
+                  <div class="carousel-item active">
+                     <div class="carousel-caption cuplle">
+                        <div class="container">
+                           <div class="row">
+                              <div class="col-md-8">
+                                 <div class="photog">
+                                    <h1>Kenali Lebih Dekat<br>Virus Corona</h1>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <!-- controls -->
+               
+               <span class="sr-only">Previous</span>
+               </a>
+               <span class="sr-only">Next</span>
+               </a>
+            </div>
+         </div>
+      </div>
+      <!-- end banner -->
+      <!-- about -->
+      <!-- end about -->
+     <!-- coronata -->
+      <div class="coronata">
+         <div class="container">
+            <div class="row d_flex grid">
+               <div class="col-md-7">
+                  <div class="coronata_img text_align_center">
+                     <figure><img src="images/about.png" alt="#"/></figure>
+                  </div>
+               </div>
+               <div class="col-md-5 oder1">
+                  <div class="titlepage text_align_left">
+                     <h2>Apa itu Virus CoronaF?</h2>
+                     <p>Corona Virus adalah kumpulan virus yang bisa menginfeksi sistem pernapasan. Pada banyak kasus, Virus ini menyebabkan infeksi pernapasan ringan, seperti flu. Namun Virus ini juga bisa menyebabkan infeksi pernapasan berat, seperti infeksi paru-paru.
+                        Virus ini disebut dengan Covid-19 dan pertama kali ditemukan dikota Wuhan, China pada akhir Desember 2019. Virus ini menular dengan sangat cepat dan telah menyebar ke hampir semua negara, termasuk Indonesia. Hanya dalam waktu beberapa bulan.  
+                     </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- end coronata -->
+   
+        <!-- protect -->
+      <div class="protect">
+         <div class="container">
+           
+       
+         </div>
+      </div>
+      <!-- end protect -->
+    <!-- cases -->
+      <div class="cases">
+         <div class="container-fluid">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage text_align_center ">
+                     <h2>Beberapa Mitos & Fakta Dari Corona Virus</h2>
+                     <p>Berikut beberapa fakta dan mitos Virus Corona yang beredar dikalangan masyarakat</p>
+                  </div>
+               </div>
+            </div>
+            <div class="row d_flex">
+               <div class=" col-md-4">
+                  <div class="latest text_align_center">
+                     <figure><img src="images/mata.jpg" alt="#"/></figure>
+                     <div class="nostrud">
+                        <h3>Mitos</h3>
+                        <h2>Sinar Matahari bisa membunuh virus Corona karena mengandung Ultraviolet</h2>
+                        <h3>Fakta</h3>
+                        <p>Tak ada bukti sinar Ultraviolet bisa membunuh Virus Corona, sinar matahari hanya memproduksi Vitamin D di dalam tubuh.</p>
+                     </div>
+                  </div>
+               </div>
+               <div class=" col-md-4">
+                  <div class="latest text_align_center">
+                     <figure><img src="images/telpon.jpg" alt="#"/></figure>
+                     <div class="nostrud">
+                        <h3>Mitos</h3>
+                        <h1>Corona menyebar lewat sinyal telepon seluler</h1>
+                        <h3>Fakta</h3>
+                        <p>Corona menyebar lewat dropet, sejauh ini, tak ditemukan virus yang bisa menyebar lewat sinyal telepon seluler</p>
+                     </div>
+                  </div>
+               </div>
+               <div class=" col-md-4">
+                  <div class="latest text_align_center">
+                     <figure><img src="images/imun.jpg" alt="#"/></figure>     
+                     <div class="nostrud">
+                        <h3>Mitos</h3>
+                        <h1>Orang tidak bisa terkena virus corona lebih dari sekali</h1>
+                        <h3>Fakta</h3>
+                        <p>Masih ada potensi terjangkit Corona setelah sembuh, meski kemungkinan lebih kecil.</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- end cases -->
+         <!-- doctors -->
+      <div class="doctors">
+         <div class="container">
+           
+         </div>
+      </div>
+      <!-- end cases -->
+<!-- update -->
+
+<!-- update -->
+      <!--  footer -->
+      <footer>
+         <div class="footer">
+            <div class="container">
+            
+            </div>
+         </div>
+      </footer>
+      <!-- end footer -->
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
+      <script src="js/owl.carousel.min.js"></script>
+      <script src="js/custom.js"></script>
+   </body>
+</html>
